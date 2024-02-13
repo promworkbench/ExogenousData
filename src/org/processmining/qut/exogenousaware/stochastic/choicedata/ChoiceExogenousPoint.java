2caac5cd16794ae12aa92f9673927e28b592e710
Adam Peter Banham <n7176546@qut.edu.au>
2024-02-13 16:53:33 +0100

    pettier impl and builder interface.
    seems stable.

--- src/org/processmining/qut/exogenousaware/stochastic/choicedata/ChoiceExogenousPoint.java
+++ src/org/processmining/qut/exogenousaware/stochastic/choicedata/ChoiceExogenousPoint.java
@@ -1,10 +1,34 @@
 package org.processmining.qut.exogenousaware.stochastic.choicedata;
 
 import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.NonNull;
 
 @Builder
 public class ChoiceExogenousPoint {
 
-	boolean known = true;
-	double value = 0.0;
+	@NonNull @Getter private String name;
+	@Default @Getter private boolean known = false;
+	@Default @Getter private double value = 0.0;
+	@Default @Getter private boolean skipped = false;
+	
+	public ChoiceExogenousPoint copy() {
+		return ChoiceExogenousPoint.builder()
+				.name(this.name)
+				.value(this.value)
+				.known(this.known)
+				.skipped(this.skipped)
+				.build();
+	}
+	
+	public String toString() {
+		if (skipped) {
+			return ">>";
+		} else if (! known) {
+			return "\bot";
+		} else {
+			return Double.toString(value);
+		}
+	}
 }
