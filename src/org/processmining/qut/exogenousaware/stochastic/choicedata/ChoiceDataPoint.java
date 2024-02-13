34fb31ca17d2ac38f39fec55f8322640fdb16a9d
Adam Peter Banham <n7176546@qut.edu.au>
2024-02-13 16:53:09 +0100

    filling out choice data point and prettier func

--- src/org/processmining/qut/exogenousaware/stochastic/choicedata/ChoiceDataPoint.java
+++ src/org/processmining/qut/exogenousaware/stochastic/choicedata/ChoiceDataPoint.java
@@ -5,12 +5,28 @@ import java.util.Set;
 import org.processmining.models.graphbased.directed.petrinet.elements.Transition;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
 
 @Builder
 public class ChoiceDataPoint {
 
-	Set<Transition> enabled;
-	Transition fired;
-	ChoiceExogenousPoint[] powers;
+	@NonNull @Getter Set<Transition> enabled;
+	@NonNull @Getter Transition fired;
+	@NonNull @Getter ChoiceExogenousPoint[] powers;
 	
+	public String toString() {
+		String ret = "(";
+		for (Transition trans : enabled) {
+			ret += trans.getLabel();
+		}
+		ret += ", <";
+		for(ChoiceExogenousPoint power : powers) {
+			ret += power.toString()+",";
+		}
+		ret += ">, ";
+		ret += fired.getLabel();
+		ret += ")";
+		return ret;
+	}
 }
