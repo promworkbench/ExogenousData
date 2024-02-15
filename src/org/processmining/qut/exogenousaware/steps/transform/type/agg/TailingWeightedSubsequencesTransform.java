2d0f63fa9ae4215e8fd4a4a5e168f0574ad6a5f1
Adam Peter Banham <n7176546@qut.edu.au>
2024-02-15 17:19:03 +0100

    tailing now weights from last event time to endo

--- src/org/processmining/qut/exogenousaware/steps/transform/type/agg/TailingWeightedSubsequencesTransform.java
+++ src/org/processmining/qut/exogenousaware/steps/transform/type/agg/TailingWeightedSubsequencesTransform.java
@@ -1,6 +1,9 @@
 package org.processmining.qut.exogenousaware.steps.transform.type.agg;
 
+import java.util.List;
+
 import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries;
+import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries.Scaling;
 import org.processmining.qut.exogenousaware.steps.transform.data.TransformedAttribute;
 import org.processmining.qut.exogenousaware.steps.transform.type.Transformer;
 
@@ -17,11 +20,15 @@ public class TailingWeightedSubsequencesTransform implements Transformer {
 	
 	@NonNull private Transformer aggerator;
 	@Default private String name = "twsub";
+	@Default private double espilion = 0.01;
 
 	public TransformedAttribute transform(SubSeries subtimeseries) {
-		double agg = 0;
+		double agg = 1;
+		List<Double> relativeTimes = subtimeseries.getXSeries(true, Scaling.min);
 		for (int k=1; k <= subtimeseries.size(); k++) {
-			int bot = (subtimeseries.getSubEvents().size() - (k - 1));
+//			int bot = (subtimeseries.getSubEvents().size() - (k - 1));
+			double bot = Math.abs(relativeTimes.get(k-1));
+			bot =+ 1.0;
 //			System.out.println("calling with bot="+bot);
 			double w = 1.0 / bot;
 			SubSeries sample = SubSeries.builder()
@@ -37,7 +44,7 @@ public class TailingWeightedSubsequencesTransform implements Transformer {
 			w = w * this.aggerator.transform(sample).getValue();
 			agg += w;
 		}
-		agg = Math.log(agg);
+		agg = Math.log(espilion+agg);
 		return new TransformedAttribute(subtimeseries.getAbvSlicingName()+"tailagg"+aggerator.getName(), agg);
 	}
 
