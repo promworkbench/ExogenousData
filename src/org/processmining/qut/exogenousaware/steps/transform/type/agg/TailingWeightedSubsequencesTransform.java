0205efdd2534473d81543f8efdb3f50b4da53e29
Adam Peter Banham <n7176546@qut.edu.au>
2024-02-22 17:17:19 +0100

    refactor for speed

--- src/org/processmining/qut/exogenousaware/steps/transform/type/agg/TailingWeightedSubsequencesTransform.java
+++ src/org/processmining/qut/exogenousaware/steps/transform/type/agg/TailingWeightedSubsequencesTransform.java
@@ -1,6 +1,8 @@
 package org.processmining.qut.exogenousaware.steps.transform.type.agg;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries;
 import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries.Scaling;
@@ -23,31 +25,57 @@ public class TailingWeightedSubsequencesTransform implements Transformer {
 	@Default private double espilion = 0.01;
 
 	public TransformedAttribute transform(SubSeries subtimeseries) {
-		double agg = 1;
+//		double agg = 1;
+//		System.out.println("starting tailing weight ("+ subtimeseries.size()+").");
 		List<Double> relativeTimes = subtimeseries.getXSeries(true, Scaling.min);
-		for (int k=1; k <= subtimeseries.size(); k++) {
-//			int bot = (subtimeseries.getSubEvents().size() - (k - 1));
-			double bot = Math.abs(relativeTimes.get(k-1));
-			bot =+ 1.0;
-//			System.out.println("calling with bot="+bot);
-			double w = 1.0 / bot;
-			SubSeries sample = SubSeries.builder()
-					.subEvents(subtimeseries.getSubEvents().subList(0, k))
-					.abvSlicingName("dummy")
-					.slicingName("dummy")
-					.dataset(subtimeseries.getDataset())
-					.source(subtimeseries.getSource())
-					.datatype(subtimeseries.getDatatype())
-					.comesFrom(subtimeseries.getComesFrom())
-					.endogenous(subtimeseries.getEndogenous())
-					.build();
-			w = w * this.aggerator.transform(sample).getValue();
-			agg += w;
-		}
+		List<Double> variances = transformToVariance(
+				subtimeseries.getYSeries(),
+				subtimeseries.getComesFrom().getMean(), 
+				subtimeseries.getComesFrom().getStd());
+		
+		double agg = IntStream.
+			rangeClosed(0,subtimeseries.size()-1)
+			.parallel()
+			.mapToDouble( (i) -> {
+				return (1/(1+Math.abs(relativeTimes.get(i)))) * variances.get(i);
+			})
+			.reduce(1.0, (ls,nx) -> ls + nx);
+			
+//		for (int k=1; k <= subtimeseries.size(); k++) {
+////			int bot = (subtimeseries.getSubEvents().size() - (k - 1));
+//			double bot = Math.abs(relativeTimes.get(k-1));
+//			bot =+ 1.0;
+////			System.out.println("calling with bot="+bot);
+//			double w = 1.0 / bot;
+//			SubSeries sample = SubSeries.builder()
+//					.subEvents(subtimeseries.getSubEvents().subList(0, k))
+//					.abvSlicingName("dummy")
+//					.slicingName("dummy")
+//					.dataset(subtimeseries.getDataset())
+//					.source(subtimeseries.getSource())
+//					.datatype(subtimeseries.getDatatype())
+//					.comesFrom(subtimeseries.getComesFrom())
+//					.endogenous(subtimeseries.getEndogenous())
+//					.build();
+//			w = w * this.aggerator.transform(sample).getValue();
+//			agg += w;
+//		}
 		agg = Math.log(espilion+agg);
+		System.out.println("finished tailing weight ("+agg+").");
 		return new TransformedAttribute(subtimeseries.getAbvSlicingName()+"tailagg"+aggerator.getName(), agg);
 	}
-
+	
+	private List<Double> transformToVariance(List<Double> values, double mean, double std){
+		List<Double> ret = new ArrayList();
+		double curr = 0;
+		for(double val : values) {
+			double now = (Math.abs(val - mean) / std);
+			ret.add(now);
+			curr += now;
+		}
+		return ret;
+	}
+		
 	public String getName() {
 		return this.name;
 	}
