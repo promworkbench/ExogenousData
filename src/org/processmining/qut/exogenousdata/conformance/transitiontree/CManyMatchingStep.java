dbbd746f9554da1aa5c862b316a0a82dafb76e01
Adam Peter Banham <n7176546@qut.edu.au>
2024-08-05 20:28:53 +1000

    fill out steps

--- src/org/processmining/qut/exogenousdata/conformance/transitiontree/CManyMatchingStep.java
+++ src/org/processmining/qut/exogenousdata/conformance/transitiontree/CManyMatchingStep.java
@@ -5,20 +5,34 @@ import java.util.Optional;
 import org.deckfour.xes.model.XEvent;
 
 public class CManyMatchingStep implements MatchingStep<XEvent, TTFlowWithGuard> {
+	
+	private XEvent event;
+	private TTFlowWithGuard flow;
+	private StepType type;
+	
+	public CManyMatchingStep(XEvent event, TTFlowWithGuard flow) {
+		super();
+		this.event = event;
+		this.flow = flow;
+		this.type = StepType.sync;
+	}
+	
+	public CManyMatchingStep(XEvent event) {
+		super();
+		this.event = event;
+		this.type = StepType.logmove;
+	}
 
 	public Optional<XEvent> getLog() {
-		// TODO Auto-generated method stub
-		return null;
+		return Optional.ofNullable(event);
 	}
 
 	public Optional<TTFlowWithGuard> getModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return Optional.ofNullable(flow);
 	}
 
 	public boolean isSkip() {
-		// TODO Auto-generated method stub
-		return false;
+		return type == StepType.logmove;
 	}
 
 }
--- src/org/processmining/qut/exogenousdata/conformance/transitiontree/MatchingStep.java
+++ src/org/processmining/qut/exogenousdata/conformance/transitiontree/MatchingStep.java
@@ -4,10 +4,28 @@ import java.util.Optional;
 
 public interface MatchingStep<T,V>{
 	
+	public static enum StepType {
+		sync,
+		logmove,
+		modelmove;
+	}
+	
+	/**
+	 * Gets the possible event for this step.
+	 * @return
+	 */
 	abstract public Optional<T> getLog();
 	
+	/**
+	 * Gets the possible flow for this step.
+	 * @return
+	 */
 	abstract public Optional<V> getModel();
 	
+	/**
+	 * Returns if the step is a skip (or a logmove).
+	 * @return
+	 */
 	abstract public boolean isSkip();
 
 }
