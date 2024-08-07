6d4153b0e35c88ba9e7947828c54eacfc4b8edc7
Adam Peter Banham <n7176546@qut.edu.au>
2024-08-07 11:22:29 +1000

    added plugins for grecall and gprecision

--- src/org/processmining/qut/exogenousdata/DataAwareConformancePlugins.java
+++ src/org/processmining/qut/exogenousdata/DataAwareConformancePlugins.java
@@ -1,11 +1,13 @@
 package org.processmining.qut.exogenousdata;
 
+import org.deckfour.xes.model.XLog;
 import org.processmining.contexts.uitopia.UIPluginContext;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginCategory;
 import org.processmining.framework.util.HTMLToString;
 import org.processmining.models.graphbased.directed.petrinetwithdata.newImpl.PetriNetWithData;
+import org.processmining.qut.exogenousdata.conformance.BookkeepingMeasures;
 import org.processmining.qut.exogenousdata.conformance.Determinism;
 import org.processmining.qut.exogenousdata.conformance.playout.PNWDPlayoutEngine;
 import org.processmining.qut.exogenousdata.conformance.transitiontree.PNWDTransitionTree;
@@ -70,14 +72,18 @@ public class DataAwareConformancePlugins {
 			parameterLabels = {"Petri net with data"},
 			returnLabels = {"DeterminismMeasure"},
 			returnTypes = {HTMLToString.class},
-			help=" Computes the determinism of a Petri with guards, which may " +
+			help=" Computes the determinism of a Petri net with guards, which may " +
 					"be a Petri net with data.\r\n" + 
 					" Determinism computes the portion of transitions that are "
 					+ "in the postset of\r\n" + 
 					" places with at least two outgoing arcs and have a "
 					+ "non-trivial guard \r\n" + 
 					" associated with them."
-					+ version,
+					+ version
+					+"<br>"
+					+ "<b>Related Work:</b> "
+					+ "Process mining with exogenous data, 2024, "
+					+ "Adam Banham, PhD Thesis, QUT, Australia.",
 			categories={PluginCategory.ConformanceChecking},
 			userAccessible = true
 	)
@@ -85,7 +91,7 @@ public class DataAwareConformancePlugins {
 			affiliation = authorAff,
 			author = authora,
 			email = authorEmail,
-			pack = "ExogenousData"
+			pack = packageName
 	)
 	public HTMLToString ComputeDeterminism(
 			final UIPluginContext context,
@@ -96,6 +102,90 @@ public class DataAwareConformancePlugins {
 				return "<p>"
 						+ "<b>Determinism:</b> " 
 						+ Determinism.ComputeDeterminism(dpn)
+						+ "</p>"; 
+			}
+		};
+	}
+	
+	@Plugin(
+			name = "Compute guard-recall of a DPN",
+			parameterLabels = {"Petri net with data","Event log"},
+			returnLabels = {"GuardRecallMeasure"},
+			returnTypes = {HTMLToString.class},
+			help=" Computes the guard-recall of a Petri net with guards, which may " +
+					"be a Petri net with data.\r\n" + 
+					" Guard-recall computes the portion of guards that were "
+					+ "true when the corsponding playout tree is traversed for"
+					+ "traces in the log (more one traversal may be considered "
+					+ "for a single trace)."
+					+ version
+					+"<br>"
+					+ "<b>Related Work:</b> "
+					+ "Comparing Conformance Checking for Decision "
+					+ "Mining: An Axiomatic Approach IEEE Access 2024, "
+					+ "Adam Banham, Arthur H. M. ter Hofstede, Sander J.J. "
+					+ "Leemans, Felix Mannhardt, Robert Andrews, Moe T. Wynn",
+			categories={PluginCategory.ConformanceChecking},
+			userAccessible = true
+	)
+	@UITopiaVariant(
+			affiliation = authorAff,
+			author = authora,
+			email = authorEmail,
+			pack = packageName
+	)
+	public HTMLToString ComputeGuardRecall(
+			final UIPluginContext context,
+			final PetriNetWithData dpn,
+			final XLog log) {
+		return new HTMLToString() {
+
+			public String toHTMLString(boolean includeHTMLTags) {
+				return "<p>"
+						+ "<b>Guard-recall:</b> " 
+						+ BookkeepingMeasures.computeGuardRecall(log, dpn)
+						+ "</p>"; 
+			}
+		};
+	}
+	
+	@Plugin(
+			name = "Compute guard-precision of a DPN",
+			parameterLabels = {"Petri net with data","Event log"},
+			returnLabels = {"GuardPrecisionMeasure"},
+			returnTypes = {HTMLToString.class},
+			help=" Computes the guard-recall of a Petri net with guards, which may " +
+					"be a Petri net with data.\r\n" + 
+					" Guard-precision computes the portion of guards that were "
+					+ "only true when for the travesed path in the corsponding "
+					+ "playout tree for traces in the log (more one traversal "
+					+ "may be considered for a single trace)."
+					+ version
+					+"<br>"
+					+ "<b>Related Work:</b> "
+					+ "Comparing Conformance Checking for Decision "
+					+ "Mining: An Axiomatic Approach IEEE Access 2024, "
+					+ "Adam Banham, Arthur H. M. ter Hofstede, Sander J.J. "
+					+ "Leemans, Felix Mannhardt, Robert Andrews, Moe T. Wynn",
+			categories={PluginCategory.ConformanceChecking},
+			userAccessible = true
+	)
+	@UITopiaVariant(
+			affiliation = authorAff,
+			author = authora,
+			email = authorEmail,
+			pack = packageName
+	)
+	public HTMLToString ComputeGuardPrecision(
+			final UIPluginContext context,
+			final PetriNetWithData dpn,
+			final XLog log) {
+		return new HTMLToString() {
+
+			public String toHTMLString(boolean includeHTMLTags) {
+				return "<p>"
+						+ "<b>Guard-recall:</b> " 
+						+ BookkeepingMeasures.computeGuardPrecision(log, dpn)
 						+ "</p>"; 
 			}
 		};
