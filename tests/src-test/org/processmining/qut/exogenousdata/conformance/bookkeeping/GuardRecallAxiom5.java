f796716f2d0a0f9fa9a3f05ba29452c43dee3977
Adam Peter Banham <n7176546@qut.edu.au>
2024-08-17 17:05:18 +1000

    added axiom testing files

---
+++ tests/src-test/org/processmining/qut/exogenousdata/conformance/bookkeeping/GuardRecallAxiom5.java
@@ -0,0 +1,28 @@
+package org.processmining.qut.exogenousdata.conformance.bookkeeping;
+
+import static org.junit.Assert.fail;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class GuardRecallAxiom5 {
+	
+	Path modelFullPath = Paths.get(
+			"tests","src-test","resource", "test_dpn_full_grec.pnml");
+	Path modelEasyPath = Paths.get(
+			"tests","src-test","resource", "paper_example_dpn_c.pnml");
+	Path modelMidPath = Paths.get(
+			"tests","src-test","resource", "paper_example_dpn_b.pnml");
+	Path modelHardPath = Paths.get(
+			"tests","src-test","resource", "paper_example_dpn_a.pnml");
+	Path logPath = Paths.get( 
+			"tests","src-test","resource", "paper_example_log.xes");
+
+	@Test
+	public void testAxiom() {
+		fail();
+	}
+
+}
