10f81bdf9489c2c7a986adf25e28b831b5e9ce07
Adam Peter Banham <n7176546@qut.edu.au>
2024-08-27 15:45:10 +1000

    added final axiom testing

---
+++ tests/src-test/org/processmining/qut/exogenousdata/conformance/bookkeeping/GuardPrecisionAxiom9.java
@@ -0,0 +1,119 @@
+package org.processmining.qut.exogenousdata.conformance.bookkeeping;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.deckfour.xes.model.XLog;
+import org.junit.Test;
+import org.processmining.models.graphbased.directed.petrinetwithdata.newImpl.PetriNetWithData;
+import org.processmining.qut.exogenousdata.conformance.playout.PNWDPlayoutEngine;
+import org.processmining.qut.exogenousdata.conformance.transitiontree.CManyMatching;
+import org.processmining.qut.exogenousdata.conformance.transitiontree.Matching;
+import org.processmining.qut.exogenousdata.conformance.transitiontree.PNWDTransitionTree;
+import org.processmining.qut.exogenousdata.utils.LoadyUtils;
+import org.processmining.qut.exogenousdata.utils.LoggyUtils;
+
+public class GuardPrecisionAxiom9 {
+	
+	Path axiomDir = Paths.get(
+			"tests","src-test","resource", "axioms", "axiom 9"
+	);
+	
+	Path modelPatha = Paths.get(axiomDir.toString(), "ax9_model_1.pnml");
+	Path modelPathb = Paths.get(axiomDir.toString(), "ax9_model_2.pnml");
+	
+	@Test
+	public void testAxiomA() {
+		PetriNetWithData model = LoadyUtils.loadDPNFromFile(
+				modelPatha.toFile()
+		);
+		if (model == null) {
+			fail("Unable to load in testing pnml file for DPN.");
+		}
+		
+		
+		List<Double> scores = new ArrayList<>();
+		
+		for(int i=1; i < 4; i++) {
+			double ret;
+			XLog log = LoadyUtils.loadLogFromFile(
+					Paths.get(axiomDir.toString(), "log_"+i+".xes").toFile()
+					
+			);
+			if (log == null) {
+				fail("Unable to load in testing log file for event log.");
+			}
+			PNWDTransitionTree tree = new PNWDTransitionTree( 
+					new PNWDPlayoutEngine().generateTraces(
+							model, LoggyUtils.findLongestTrace(log)
+					) 
+			);
+			ExploratoryBookkeepingEngine engine = new ExploratoryBookkeepingEngine();
+			Matching matcher = new CManyMatching(tree);
+			ret = engine.computeBookkeeping(tree, matcher, log);
+			System.out.println(
+					"[Axiom 9] Computed guard-recall ("
+					+"using log "
+					+ i
+					+") was :: " 
+					+ ret);
+			scores.add(ret);
+		}
+		
+		for(int i=0; i <scores.size()-1; i++) {
+			for(int j=i+1; j < scores.size(); j++) {
+				assertEquals(scores.get(i), scores.get(j), 1e-4);
+			}
+		}
+	}
+	
+	@Test
+	public void testAxiomB() {
+		PetriNetWithData model = LoadyUtils.loadDPNFromFile(
+				modelPathb.toFile()
+		);
+		if (model == null) {
+			fail("Unable to load in testing pnml file for DPN.");
+		}
+		
+		
+		List<Double> scores = new ArrayList<>();
+		
+		for(int i=1; i < 4; i++) {
+			double ret;
+			XLog log = LoadyUtils.loadLogFromFile(
+					Paths.get(axiomDir.toString(), "log_"+i+".xes").toFile()
+					
+			);
+			if (log == null) {
+				fail("Unable to load in testing log file for event log.");
+			}
+			PNWDTransitionTree tree = new PNWDTransitionTree( 
+					new PNWDPlayoutEngine().generateTraces(
+							model, LoggyUtils.findLongestTrace(log)
+					) 
+			);
+			ExploratoryBookkeepingEngine engine = new ExploratoryBookkeepingEngine();
+			Matching matcher = new CManyMatching(tree);
+			ret = engine.computeBookkeeping(tree, matcher, log);
+			System.out.println(
+					"[Axiom 9] Computed guard-recall ("
+					+"using log "
+					+ i
+					+") was :: " 
+					+ ret);
+			scores.add(ret);
+		}
+		
+		for(int i=0; i <scores.size()-1; i++) {
+			for(int j=i+1; j < scores.size(); j++) {
+				assertEquals(scores.get(i), scores.get(j), 1e-4);
+			}
+		}
+	}
+}
